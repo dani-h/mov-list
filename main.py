26ee67a98be26c666dbfbfb80b887408c811e9e5
Dani Hodovic <danihgit@gmail.com>
2015-04-25 23:14:16 +0200

    Initial models, SQLAlchemy config
    SQLAlchemy configured for a local sqlite db with thread local sessions,
    does not use the flask-sqlalchemy library.
    Initial models with simple titles added.
    Simple restful API for posting and getting movies.

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+import os
+import flask
+from flask import Flask, request, render_template
+from models import Session, Movie
+
 app = Flask(__name__, static_url_path='')
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    Session.remove()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/movie', methods=['GET'])
+@app.route('/movie/<id>', methods=['GET'])
+def get_movies(id=None):
+    if id is not None:
+        movies = Movie.query.filter(Movie.id == id).all()
+    else:
+        movies = [mov.as_dict() for mov in Movie.query]
+
+    return flask.jsonify({'movies': movies})
+
+
+@app.route('/movie/', methods=['POST'])
+def add_movie():
+    title = request.form.get('title')
+    if title:
+        movie = Movie(title=title)
+        Session().add(movie)
+        Session().commit()
+        return flask.jsonify({'title': title}), 201
+    else:
+        return "Invalid request", 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, MetaData, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+DB_PATH = os.path.dirname(__file__) + "/mov.db"
+engine = create_engine('sqlite:////' + DB_PATH, convert_unicode=True)
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+metadata = MetaData()
+metadata.create_all(bind=engine)
+
+class BaseCls(object):
+    @property
+    def columns(self):
+        # pylint: disable=no-member
+        return [col.name for col in self.__table__.columns]
+
+    def as_dict(self):
+        return dict([ (col, getattr(self, col)) for col in self.columns])
+
+Base = declarative_base(cls=BaseCls)
+# Convenient access to query using MyClass.query, but pylint complains...
+Base.query = Session.query_property()
+
+
+# ---------------------------------------------
+# Models
+# ---------------------------------------------
+
+class Movie(Base):
+    __tablename__ = 'movie'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80))
